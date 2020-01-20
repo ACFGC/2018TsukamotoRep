daaff4f85c71ba2e516f0a105f29b492500df106
saboten <saboten1472580@gmail.com>
2020-01-20 09:59:03 +0900

    radar

---
+++ 3D_ゲーム後期 戦闘機 ヘリ チュートリアル8/GameProgramming/CRadar.h
@@ -0,0 +1,19 @@
+#ifndef CRADAR_H
+#define CRADAR_H
+#include"CCollider.h"
+#include"CCharacter.h"
+#include"CBox.h"
+class CRadar :public CCharacter{
+public:
+	CTriangle mT[2];
+	CBox mBox;
+	CCollider mCollider;
+
+	CRadar();
+	void Set(float x, float y, float z, float w, float h, float d);
+	void SetDiffuse(float r, float g, float b, float a);
+	void Update();
+	void Render();
+	void Collision(CCollider *m, CCollider *y);
+};
+#endif
